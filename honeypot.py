57f5aa952b6d0650db813d6532c716dc963cd469
cami-santoro <70799750+cami-santoro@users.noreply.github.com>
2023-12-07 12:41:55 +0100

    New features

--- honeypot.py
+++ honeypot.py
@@ -6,7 +6,7 @@ def login():
     conn.send("ubuntu login:".encode())
     username = ""
     while True:
-        data = conn.recv(1) #Receive username from the socket.
+        data = conn.recv(1) #Receive username from the socket
         if (data.decode()=='\n'):
             break
         else:
@@ -17,7 +17,7 @@ def login():
     conn.sendall("password:".encode())
     password=""
     while True:
-        data = conn.recv(1) #Receive password from the socket.
+        data = conn.recv(1) #Receive password from the socket
         if (data.decode()=='\n'):
             break
         else:
@@ -47,27 +47,68 @@ print("Connection approved, user:", client_info)
 conn.send('Ubuntu 22.04.2 LTS \r\n'.encode())
 username, password=login()
 loginLog(client_info, username, password)
-welcome_text='Welcome '+username
-conn.send(welcome_text.encode())
-conn.sendall("admin@ubuntu:~$ ".encode())
-while True:
+conn.sendall("Welcome ".encode())
+conn.sendall(username.encode())
+conn.send('\r\n'.encode())
+exit=False
+current_dir="home"
+while (not exit):
+    if(current_dir=="home"):
+        conn.sendall("admin@ubuntu:~$ ".encode())
+    elif(current_dir=="Desktop"):
+        conn.sendall("admin@ubuntu:~/Desktop$ ".encode())
+    elif(current_dir=="Documents"):
+        conn.sendall("admin@ubuntu:~/Documents$ ".encode())
+    elif(current_dir=="Downloads"):
+        conn.sendall("admin@ubuntu:~/Downloads$ ".encode())
+    elif(current_dir=="Pictures"):
+        conn.sendall("admin@ubuntu:~/Pictures$ ".encode())
+    elif(current_dir=="Public"):
+        conn.sendall("admin@ubuntu:~/Public$ ".encode())
+    elif(current_dir=="Videos"):
+        conn.sendall("admin@ubuntu:~/Videos$ ".encode())
+    
     command=""
+    # --
     while True:
-        data = conn.recv(1) #Receive command from the socket.
+        data = conn.recv(1) #Receive command from the socket
         if (data.decode()=='\n'):
             break
         else:
             command += data.decode("utf-8")
+    # --
+    print(command.strip())
+    commandsLog(command) #save the command
     if (command.strip()=='exit'):
-        break
-    commandsLog(command)
-    #TO DO 
-    '''if(command.strip()=='ls'):
-        fakeDirectory=''
-    elif (command.strip()=='cat /etc/*-release'):
-        fake_distro="DISTRIB_ID=Ubuntu\r\nDISTRIB_RELEASE=22.04\r\nDISTRIB_CODENAME=jammy\r\nDISTRIB_DESCRIPTION=\"Ubuntu 22.04.2 LTS\"\r\nPRETTY_NAME=\"Ubuntu 22.04.2 LTS\"\r\nNAME=\"Ubuntu\"\r\nVERSION_ID=\"22.04\"\r\nVERSION=\"22.04.2 LTS (Jammy Jellyfish)\r\n\"VERSION_CODENAME=jammy\r\nID=ubuntu\r\nID_LIKE=debian\r\nHOME_URL=\"https://www.ubuntu.com/\"\r\nSUPPORT_URL=\"https://help.ubuntu.com/\"\r\nBUG_REPORT_URL=\"https://bugs.launchpad.net/ubuntu/\"\r\nPRIVACY_POLICY_URL=\"https://www.ubuntu.com/legal/terms-and-policies/privacy-policy\"\r\nUBUNTU_CODENAME=jammy"
-    elif(command.strip()=='uname -r'):
-'''
+        exit=True
+    elif(current_dir=='home' and command.strip()=='ls'):
+        fakeDirectory='Desktop\r\nDocuments\r\nDownloads\r\nPictures\r\nPublic\r\nVideos'
+        conn.send(fakeDirectory.encode())
+    elif (command.strip()=="cat /etc/*-release"):
+        fake_distro="DISTRIB_ID=Ubuntu\r\nDISTRIB_RELEASE=22.04\r\nDISTRIB_CODENAME=jammy\r\nDISTRIB_DESCRIPTION=\"Ubuntu 22.04.2 LTS\"\r\nPRETTY_NAME=\"Ubuntu 22.04.2 LTS\"\r\nNAME=\"Ubuntu\"\r\nVERSION_ID=\"22.04\"\r\nVERSION=\"22.04.2 LTS (Jammy Jellyfish)\r\n\"VERSION_CODENAME=jammy\r\nID=ubuntu\r\nID_LIKE=debian\r\nHOME_URL=\"https://www.ubuntu.com/\"\r\nSUPPORT_URL=\"https://help.ubuntu.com/\"\r\nBUG_REPORT_URL=\"https://bugs.launchpad.net/ubuntu/\"\r\nPRIVACY_POLICY_URL=\"https://www.ubuntu.com/legal/terms-and-policies/privacy-policy\"\r\nUBUNTU_CODENAME=jammy\r\n"
+        conn.send(fake_distro.encode())
+    elif(command.strip()=="uname -r"):
+        fake_version="5.19.0-42-generic\r\n"
+        conn.send(fake_version.encode())
+    elif (command.strip()=="cd Desktop"):
+        current_dir="Desktop"
+    elif (command.strip()=="cd Documents"):
+        current_dir="Documents"
+    elif (command.strip()=="cd Downloads"):
+        current_dir="Downloads"
+    elif (command.strip()=="cd Pictures"):
+        current_dir="Pictures"
+    elif (command.strip()=="cd Public"):
+        current_dir="Public"
+    elif (command.strip()=="cd Videos"):
+        current_dir="Videos"
+    elif(command.strip()=="cd .."):
+        current_dir="home"
+    else:
+        conn.send("Command not found\r\nTry '--help' for more information\r\n".encode())
+
+
+
 
      
 
